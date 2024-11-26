d5c47969d0ca9145d84c9d0222282e63ed0c1f89
Sakshi <sakshi.agg428@gmail.com>
2024-11-26 13:41:05 +0530

    Purchase Product flow

--- src/test/java/com/MyStore/testcases/TestProductPage.java
+++ src/test/java/com/MyStore/testcases/TestProductPage.java
@@ -37,14 +37,17 @@ public class TestProductPage extends BaseTest {
 
         ProductsPage pg = new ProductsPage(driver);
         pg.clickOnProducts();
+        logger.info("Enter product to search...");
         pg.enterTextInSearchBar(searchKey);
         pg.clickOnSearchIcon();
         scrollWindow(0, 450);
 //        String productName = pg.verifyProductDescription();
 //        System.out.println(productName);
+        logger.info("Scroll the window and click on view product");
         pg.clickOnViewProduct();
         String productName = pg.getProductName();
         System.out.println(productName);
+        logger.info("Verify the name of the product");
         if (productName.contains(searchKey)) {
             logger.info("Verify product - Passed");
             Assert.assertTrue(true);
@@ -53,24 +56,31 @@ public class TestProductPage extends BaseTest {
             captureScreenshot(driver, "verifyProduct");
             Assert.assertTrue(false);
         }
+        logger.info("Enter the quantity of products");
         pg.enterQty("2");
+        logger.info("Add product to the cart");
         pg.clickOnAddToCart();
         CartPage cp = new CartPage(driver);
 //        driver.switchTo().frame("Added!");
 //        String text = cp.verifyProductAdded();
 //        Assert.assertEquals("Added!", text);
+        logger.info("View cart details and checkout");
         cp.clickOnCartDetails();
         cp.clickOnCheckout();
         OrderPage op = new OrderPage(driver);
+        logger.info("Click on place order");
         op.clickOnPlaceOrder();
         paymentPage = new PaymentPage(driver);
+        logger.info("Enter card details");
         orderDetailsPage = paymentPage.goToOrderDetails("Aviel Mansour", "4932971218528393", "115", "11", "2027");
+        logger.info("Review order which you placed...");
         String orderDetails = orderDetailsPage.verifyOrderPlaced();
         Assert.assertEquals("ORDER PLACED!", orderDetails);
+        logger.info("Download Invoice");
         orderDetailsPage.getInvoice();
+        logger.info("Click on continue");
         orderDetailsPage.clickOnContinue();
-
-
+        logger.info("After purchase => Logout");
         homePage.clickOnLogout();
     }
 
